37de0d4d9255371bd12d585395eab1e91f678e23
zsj1029 <zsj1029@gmail.com>
2022-11-25 18:01:38 +0800

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/vite-mithril-jsx/",
 	esbuild: {
 		jsx: "transform",
 		jsxFactory: "m",
